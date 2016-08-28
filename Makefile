c016195d71318e0473191334552ab40048311c97
himu62 <zero.256dic@gmail.com>
2016-08-28 17:15:32 +0900

    add environment files

---
+++ Makefile
@@ -0,0 +1,30 @@
+.PHONY: more \
+	server server/test server/build server/deps \
+	client/build client/watch client/deps
+
+# http://marmelab.com/blog/2016/02/29/auto-documented-makefile.html
+more:
+	@awk 'BEGIN {FS = ":.*?## "} /^[\$$\(\)\/a-zA-Z_-]+:.*?## / {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+
+server:                                           ## サーバを起動
+	go run lp.go
+
+server/test:                                      ## サーバをテスト
+	go test -v ./...
+
+server/build:                                     ## サーバをビルド
+	go build -o hearb-lp lp.go
+
+server/deps:                                      ## サーバの依存パッケージをダウンロード
+	go get -d -t -v ./...
+	go get -v github.com/rubenv/sql-migrate/...
+
+client/build:                                     ## クライアントをビルド
+	NODE_ENV=production ./node_modules/.bin/webpack
+
+client/watch:                                     ## クライアントを自動コンパイル
+	NODE_ENV=development ./node_modules/.bin/webpack --watch
+
+client/deps:                                      ## クライアントの依存パッケージをダウンロード
+	npm install
+	./node_modules/.bin/typings install
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var plugins = [];
+var css_loader = 'css-loader!sass-loader';
+if(process.env.NODE_ENV === 'production') {
+	plugins = [
+		new webpack.optimize.UglifyJsPlugin(),
+		new webpack.optimize.DedupePlugin(),
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		})
+	];
+	css_loader = 'css-loader?minimize!sass-loader';
+}
+
+module.exports = [
+	{
+		entry: path.join(__dirname, 'script/index.tsx'),
+		output: {
+			filename: 'public/js/bundle.js'
+		},
+		resolve: {
+			extensions: ['', '.tsx', '.ts', '.js']
+		},
+		module: {
+			loaders: [
+				{
+					test: /\.tsx?$/,
+					loader: 'ts-loader',
+					exclude: /node_modules/
+				}
+			]
+		},
+		plugins: plugins
+	},
+	{
+		entry: path.join(__dirname, 'style/style.scss'),
+		output: {
+			filename: 'public/css/style.css'
+		},
+		resolve: {
+			extensions: ['', '.scss', '.css']
+		},
+		module: {
+			loaders: [
+				{
+					test: /\.scss$/,
+					loader: ExtractTextPlugin.extract('style-loader', css_loader)
+				}
+			]
+		},
+		plugins: [
+			new ExtractTextPlugin('public/css/style.css')
+		]
+	}
+];
